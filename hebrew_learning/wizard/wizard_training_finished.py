b129c1d516fe024738956187a94a99946e5cfd58
Vitaly Kazimirov <kazimirows@yandex.by>
2021-09-01 17:50:14 +0300

    6 new verbs added + random image on success wizard

--- hebrew_learning/wizard/wizard_training_finished.py
+++ hebrew_learning/wizard/wizard_training_finished.py
@@ -1,4 +1,5 @@
 import base64
+import random
 from odoo import models, fields, _
 from odoo import modules
 
@@ -8,7 +9,15 @@ class WizardTrainingFinished(models.TransientModel):
     _description = 'Wizard for finished training'
 
     def get_default_img():
-        with open(modules.get_module_resource('hebrew_learning', 'static/img', 'image_success.png'),'rb') as f:
+        few_success_image_names = (
+            'image_success_1.png',
+            'image_success_2.png',
+            'image_success_3.png',
+            'image_success_4.png',
+            'image_success_5.png',
+        )
+        success_image = random.choice(few_success_image_names)
+        with open(modules.get_module_resource('hebrew_learning', 'static/img', success_image),'rb') as f:
             return base64.b64encode(f.read())
 
     success_image = fields.Binary(
