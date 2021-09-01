05f7ea9d962e31d0d51b660ddd444ca6daf76403
Vitaly Kazimirov <kazimirows@yandex.by>
2021-09-01 23:25:45 +0300

    Renamed picture

--- hebrew_learning/wizard/wizard_training_finished.py
+++ hebrew_learning/wizard/wizard_training_finished.py
@@ -1,5 +1,4 @@
 import base64
-import random
 from odoo import models, fields, _
 from odoo import modules
 
@@ -9,15 +8,7 @@ class WizardTrainingFinished(models.TransientModel):
     _description = 'Wizard for finished training'
 
     def get_default_img():
-        few_success_image_names = (
-            'image_success_1.png',
-            'image_success_2.png',
-            'image_success_3.png',
-            'image_success_4.png',
-            'image_success_5.png',
-        )
-        success_image = random.choice(few_success_image_names)
-        with open(modules.get_module_resource('hebrew_learning', 'static/img', success_image),'rb') as f:
+        with open(modules.get_module_resource('hebrew_learning', 'static/img', 'image_success_2.png'),'rb') as f:
             return base64.b64encode(f.read())
 
     success_image = fields.Binary(
