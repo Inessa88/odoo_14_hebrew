a9cf901ea113d01e71b0d46b492993762b68edcf
Vitaly <kazimirofs@gmail.com>
2022-05-23 23:46:10 +0300

    EC2 with low resources

--- hebrew_learning/__manifest__.py
+++ hebrew_learning/__manifest__.py
@@ -22,11 +22,11 @@
                 
         'data/exercise_types_data.xml',
         'data/word_groups_and_words_data_1.xml',
-        'data/word_groups_and_words_data_2.xml',
+        # 'data/word_groups_and_words_data_2.xml',
         # 'data/word_groups_and_words_data_3.xml',
         # 'data/word_groups_and_words_data_4.xml',
         # 'data/word_groups_and_words_data_5.xml',
-        'data/sentences_data_1.xml',
+        # 'data/sentences_data_1.xml',
         # 'data/sentences_data_2.xml',
 
         'wizard/learning.xml',
